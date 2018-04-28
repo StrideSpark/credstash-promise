3dd9ad008e0fdef66ef80a86b4aeb925502d9d58
Megan <megan@stridespark.com>
2018-04-28 16:02:10 -0700

    updated interface and added prettier etc

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,96 @@
+import Credstash = require('nodecredstash');
+import * as _ from 'lodash';
 
-const Credstash = require('@stridespark/credstash');
-const credstash = new Credstash();
+const credstash = Credstash({
+    awsOpts: {
+        region: 'us-west-2',
+    },
+});
 
-export function fetchCred(name: string): Promise<string> {
-    return new Promise<string>(
-        (resolve, reject) =>
-            credstash.get(name, (err: any, secret: string) => {
-                if (err) reject(err);
-                else resolve(secret);
-            })
-    );
-}
-
-export async function fetchAppCred(env: string, appName: string, name: string, defaultValue: string): Promise<string> {
+/*
+Fetch specific secrets
+*/
+export async function fetchCred(name: string): Promise<string | undefined> {
     try {
-        const appVal = fetchCred(`${env}.${appName}.${name}`)
-        const result = await appVal;
+        const val = credstash.getSecret({ name });
+        const result = await val;
         return result;
     } catch (err) {
         // if a different error, return it
-        if (err.message.indexOf('secret not found') === -1) {
+        if (err.message.indexOf('could not be found') === -1) {
             throw err;
         }
     }
+}
+
+export async function fetchAppCred(env: string, appName: string, name: string): Promise<string> {
+    const appVal = await fetchCred(`${env}.${appName}.${name}`);
+    if (appVal != undefined) return appVal;
+
+    const envVal = await fetchCred(`${env}.${name}`);
+    if (envVal != undefined) return envVal;
+
+    const defaultVal = await fetchCred(name);
+    if (defaultVal != undefined) return defaultVal;
+
+    throw Error('no secrets match env=' + env + ', appName=' + appName + ', name=' + name);
+}
+
+export async function fetchCredHistory(name: string): Promise<{ version: string; secret: string }[]> {
     try {
-        const envVal = fetchCred(`${env}.${name}`);
-        return await envVal;
+        const vals = credstash.getAllVersions({ name });
+        const results = await vals;
+        return results;
     } catch (err) {
         // if a different error, return it
         if (err.message.indexOf('secret not found') === -1) {
             throw err;
         }
+        return [];
     }
-    return defaultValue;
+}
+
+/*
+List secrets
+*/
+export async function listAllSecretsAllVersions(contains?: string): Promise<{ name: string; version: string }[]> {
+    const results: { name: string; version: string }[] = (await credstash.listSecrets()) as any;
+    if (contains != undefined) {
+        return results.filter(r => r.name.toLowerCase().indexOf(contains.toLowerCase()) != -1);
+    }
+    return results;
+}
+
+export async function listSecrets(contains?: string): Promise<string[]> {
+    const results = await listAllSecretsAllVersions(contains);
+    return _.uniq(results.map(r => r.name));
+}
+
+/*
+Modify secrets
+*/
+
+export async function putSecret(name: string, secret: string): Promise<{ name: string; version: string }> {
+    try {
+        await credstash.putSecret({ name, secret, context: {} });
+        return { name, version: '0000000000000000001' };
+    } catch (err) {
+        if (err.message.indexOf('is already in the credential store') != -1) {
+            const newVersion = await credstash.incrementVersion({ name });
+            await credstash.putSecret({ name, secret, context: {}, version: +newVersion });
+            return { name, version: newVersion };
+        }
+        throw err;
+    }
+}
+
+export async function deleteSecret(name: string, version?: string) {
+    if (version != undefined) {
+        if (isNaN(Number(version))) {
+            throw Error('version must be a number, not ' + version);
+        }
+        await credstash.deleteSecret({ name, version: Number(version) });
+    } else {
+        await credstash.deleteSecrets({ name });
+    }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,36 +1,133 @@
-import { fetchAppCred } from "../src/index";
+import { fetchAppCred, fetchCredHistory, fetchCred, listSecrets, listAllSecretsAllVersions, putSecret, deleteSecret } from '../src/index';
 import { assert } from 'chai';
-import * as AWS from 'aws-sdk';
-AWS.config.region = 'us-west-2';
 
-describe('credstash-promise', function () {
-    it('fetches app-level creds', async function () {
-        const result = await fetchAppCred('test', 'credstash-promise', 'testval.foo', 'default');
-        assert.equal(result, 'applevel');
-    })
+describe('credstash-promise', function() {
+    describe('fetch app cred', function() {
+        it('fetches app-level creds', async function() {
+            const result = await fetchAppCred('test', 'credstash-promise', 'testval.foo');
+            assert.equal(result, 'applevel');
+        });
 
-    it('fetches env-level creds when app has nothing', async function () {
-        const result = await fetchAppCred('test', 'bad-app', 'testval.foo', 'default');
-        assert.equal(result, 'envlevel');
-    })
+        it('fetches env-level creds when app has nothing', async function() {
+            const result = await fetchAppCred('test', 'bad-app', 'testval.foo');
+            assert.equal(result, 'envlevel');
+        });
 
+        it('fetches default creds when env and app has nothing', async function() {
+            const result = await fetchAppCred('bad-env', 'bad-app', 'testval.foo');
+            assert.equal(result, 'default4');
+        });
 
-    it('fetches default creds when env and app has nothing', async function () {
-        const result = await fetchAppCred('bad-env', 'bad-app', 'testval.foo', 'default');
-        assert.equal(result, 'default');
-    })
+        it('throws when no matching values', async function() {
+            try {
+                await fetchAppCred('not', 'in', 'existence');
+            } catch (err) {
+                assert.match(err, /no secrets match/);
+                return;
+            }
+            assert.fail();
+        });
+    });
 
+    describe('fetchCred', function() {
+        it('does not throw when missing values', async function() {
+            const result = await fetchCred('not a thing');
+            assert.isUndefined(result);
+        });
 
-    it('throws on errors besides missing secrets', async function () {
-        AWS.config.region = undefined;
-        let hasErrored = false;
-        try {
-            await fetchAppCred('bad-env', 'bad-app', 'testval.foo', 'default');
-        } catch (err) {
-            assert.equal(err.message, 'Missing region in config')
-            hasErrored = true;
-        }
-        assert.isTrue(hasErrored)
-    })
+        it('does throw for other reasons', async function() {
+            try {
+                await fetchCred(undefined as any);
+            } catch (err) {
+                assert.match(err, /name is a required parameter/);
+                return;
+            }
+            assert.fail();
+        });
+    });
 
-})
+    describe('fetch cred history', function() {
+        it('as expected', async function() {
+            const results = await fetchCredHistory('testval.foo');
+            assert.deepEqual(results, [
+                { version: '0000000000000000004', secret: 'default4' },
+                { version: '0000000000000000003', secret: 'default3' },
+                { version: '0000000000000000002', secret: 'default2' },
+                { version: '0000000000000000001', secret: 'default' },
+            ]);
+        });
+
+        it('no val', async function() {
+            const results = await fetchCredHistory('not a val');
+            assert.lengthOf(results, 0);
+        });
+
+        it('throws if no name', async function() {
+            try {
+                await fetchCredHistory(undefined as any);
+            } catch (err) {
+                assert.match(err, /name is a required parameter/);
+                return;
+            }
+            assert.fail();
+        });
+    });
+
+    describe('list secrets', function() {
+        it('list all secrets - no filter', async function() {
+            const results = await listAllSecretsAllVersions();
+            assert.isAtLeast(results.length, 100);
+            assert.includeDeepMembers(results, [
+                { name: 'testval.foo', version: '0000000000000000004' },
+                { name: 'testval.foo', version: '0000000000000000003' },
+                { name: 'testval.foo', version: '0000000000000000002' },
+                { name: 'testval.foo', version: '0000000000000000001' },
+            ]);
+        });
+
+        it('list all secrets - filter', async function() {
+            const results = await listAllSecretsAllVersions('foo');
+            assert.isAtLeast(results.length, 4);
+            assert.includeDeepMembers(results, [
+                { name: 'testval.foo', version: '0000000000000000004' },
+                { name: 'testval.foo', version: '0000000000000000003' },
+                { name: 'testval.foo', version: '0000000000000000002' },
+                { name: 'testval.foo', version: '0000000000000000001' },
+            ]);
+        });
+
+        it('list secrets - no filter', async function() {
+            const results = await listSecrets();
+            assert.isAtLeast(results.length, 10);
+            assert.includeMembers(results, ['testval.foo']);
+        });
+        it('list secrets - filter', async function() {
+            const results = await listSecrets('testval.');
+            assert.isAtLeast(results.length, 1);
+            assert.includeMembers(results, ['testval.foo']);
+        });
+    });
+
+    describe('modify secrets', function() {
+        it('as expected', async function() {
+            //start by deleting all versions of 'hi'
+            await deleteSecret('hi');
+
+            //put two values in for 'hi':
+            let result = await putSecret('hi', 'there');
+            assert.deepEqual(result, { name: 'hi', version: '0000000000000000001' });
+            assert.equal(await fetchCred('hi'), 'there');
+            result = await putSecret('hi', 'there2');
+            assert.deepEqual(result, { name: 'hi', version: '0000000000000000002' });
+            assert.equal(await fetchCred('hi'), 'there2');
+
+            //delete the most recent version and make sure the previous version is still there:
+            await deleteSecret('hi', '2');
+            assert.equal(await fetchCred('hi'), 'there');
+
+            //delete all versions and make sure nothing is there:
+            await deleteSecret('hi');
+            assert.isUndefined(await fetchCred('hi'));
+        });
+    });
+});
